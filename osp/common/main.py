419e878d7072e465fe73b4ef2c0650d73f9d8d3a
Veeresh Aradhya <varadhya@redhat.com>
2024-03-21 22:34:33 +0530

    Adding main.py to use the api

---
+++ osp/common/main.py
@@ -0,0 +1,35 @@
+from osp.api.pipeline import Pipeline
+from osp.api.pipelinerun import PipelineRun
+from osp.util.subscription import Subscription
+from kubernetes.dynamic.exceptions import ResourceNotFoundError
+
+
+class OSPBase:
+    def __init__(self):
+        # Initialising all the class variables
+        for attr in dir(self):
+            if not attr.startswith("_"):
+                member = getattr(self, attr)
+                try:
+                    setattr(self, attr, member())
+                except ResourceNotFoundError:
+                    # Skipping creation of instance for the resources which are not present yet.
+                    pass
+
+
+class API(OSPBase):
+    pipelines = Pipeline
+    pipelinerun = PipelineRun
+
+
+class Utils(OSPBase):
+    subscriptions = Subscription
+
+
+class OSPMain(OSPBase):
+    api = API
+    utils = Utils
+
+
+if __name__ == "__main__":
+    osp = OSPMain()
